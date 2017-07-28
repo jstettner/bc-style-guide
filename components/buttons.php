96907bc1709ac8a17e2232f72f1368f4bba9a587
Jack Stettner <johndoe@example.com>
2017-07-28 15:25:01 -0400

    modularization

---
+++ components/buttons.php
@@ -0,0 +1,18 @@
+<div id="buttons" class="prm pll">
+  <h3> Buttons </h3>
+  <div class="flex-row flex-wrap">
+    <button type="button" name="button" class="button-primary type-med mh-10 mv-10"> Primary </button>
+    <button type="button" name="button" class="button-danger type-med mh-10 mv-10"> Danger </button>
+    <button type="button" name="button" class="button-default type-med mh-10 mv-10"> Default </button>
+    <button type="button" name="button" class="button-success type-med mh-10 mv-10"> Success </button>
+    <button type="button" name="button" class="button-transferred type-med mh-10 mv-10"> Transferred </button>
+    <button type="button" name="button" class="button-received type-med mh-10 mv-10"> Received </button>
+    <button type="button" name="button" class="button-success-alt type-med mh-10 mv-10"> Success-alt </button>
+    <button type="button" name="button" class="button-white-inverse type-med mh-10 mv-10"> White-inverse </button>
+    <button type="button" name="button" class="button-primary-inverse type-med mh-10 mv-10"> Primary-inverse </button>
+    <button type="button" name="button" class="button-purple type-med mh-10 mv-10"> Purple </button>
+    <button type="button" name="button" class="button-info type-med mh-10 mv-10" > Info </button>
+    <button type="button" name="button" class="button-blue type-med mh-10 mv-10"> Blue </button>
+    <button type="button" name="button" class="button-sent type-med mh-10 mv-10"> Sent </button>
+  </div>
+</div>
