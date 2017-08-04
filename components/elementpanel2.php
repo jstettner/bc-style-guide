28c7535692e8015a64772f8acfe8b90cd6966e72
Jack Stettner <johndoe@example.com>
2017-08-04 16:23:19 -0400

    panel 1 and 2 build

---
+++ components/elementpanel2.php
@@ -0,0 +1,89 @@
+<div class="flex-row ph-80 flex-column-mobile">
+  <div class="flex-column width-50 width-50-tablet width-100-mobile pr-80 ph-30-tablet pln">
+    <span class="em-500">Buttons</span>
+    <hr>
+    <span class="em-300 pb-15">PRIMARY BUTTON LARGE</span>
+    <div class="flex-row flex-between">
+      <div class="flex-column width-50 pr-45">
+        <button type="button" class="button-primary width-100" name="button">0.110253 BTC</button>
+        <span class="pt-5">Active - spans width of fields</span>
+      </div>
+      <div class="flex-column width-50 pr-45">
+        <button type="button" class="button-primary width-100" name="button" disabled="disabled">0.110253 BTC</button>
+        <span class="pt-5">Inactive - spans width of fields</span>
+      </div>
+    </div>
+    <span class="em-300 pt-30 pb-15">PRIMARY BUTTON</span>
+    <div class="flex-row flex-start">
+      <div class="flex-column pr-45">
+        <button type="button" class="button-primary" name="button">CONTINUE</button>
+        <span class="pt-5">Active</span>
+      </div>
+      <div class="flex-column">
+        <button type="button" class="button-primary" name="button" disabled="disabled">CONTINUE</button>
+        <span class="pt-5">Inactive</span>
+      </div>
+    </div>
+    <span class="em-300 pt-30 pb-15">SECONDARY BUTTON</span>
+    <div class="flex-row flex-between flex-column-tablet flex-column-mobile">
+      <div class="flex-column">
+        <button type="button" class="button-sent" name="button">0.110253 BTC</button>
+        <span class="pt-5">Sent</span>
+        <button type="button" class="button-sent mt-20" name="button" disabled="disabled">0.110253 BTC</button>
+        <span class="pt-5">Pending Sent</span>
+      </div>
+      <div class="flex-column">
+        <button type="button" class="button-received" name="button">0.110253 BTC</button>
+        <span class="pt-5">Received</span>
+        <button type="button" class="button-received mt-20" name="button" disabled="disabled">0.110253 BTC</button>
+        <span class="pt-5">Pending Received</span>
+      </div>
+      <div class="flex-column">
+        <button type="button" class="button-transferred" name="button">0.110253 BTC</button>
+        <span class="pt-5">Transferred</span>
+        <button type="button" class="button-transferred mt-20" name="button" disabled="disabled">0.110253 BTC</button>
+        <span class="pt-5">Pending Transferred</span>
+      </div>
+    </div>
+    <span class="em-300 pt-30 pb-15">OTHER BUTTONS</span>
+    <div class="flex-row">
+      <button type="button" class="btn button-default button-short-wide type-h5 mrm">
+        <i class="mrs media-middle icon-send"></i>
+        <span class="em-300">Send</span>
+      </button>
+      <button class="btn button-default button-short-wide type-h5 pos-rel">
+        <i class="mrs media-middle icon-receive"></i>
+        <span class="em-300 ng-scope">Request</span>
+      </button>
+    </div>
+    <span class="em-300 pt-30 ">LINKS</span>
+    <a class="em-300 pt-5 pb-15 primary">Download iOS app</a>
+  </div>
+
+  <div class="flex-column width-50 width-50-tablet width-100-mobile pr-80 ph-30-tablet pln">
+    <span class="em-500">Forms</span>
+    <hr>
+    <span class="em-600 pb-5">Label - 5px above field</span>
+    <input class="form-control mb-15" style="border-color:#DDD;background-color:white;cursor:text;" value="Input value" readonly="readonly">
+    <input class="form-control mb-15" style="border-color:#DDD;background-color:white;" placeholder="Placeholder...">
+    <input class="form-control mb-15" style="border-color:#039bd3;background-color:white;" placeholder="Focus">
+    <input class="form-control" style="border-color:#b83940;background-color:white;" placeholder="Error">
+    <span class="help-block">
+      <div class="" style="">
+        <p class="ng-scope ng-hide" style="">Error messaging - 5px below field</p>
+      </div>
+    </span>
+    <form class="bc-form">
+      <div class="radio-group">
+        <div class="radio-label">
+          <input name="rbutton" type="radio" id="1">
+          <label for="1" style="border-bottom-width: 0;">Radio Label</label>
+        </div>
+        <div class="radio-label">
+          <input name="rbutton" type="radio" id="2">
+          <label for="2" style="border-top-width: 0;">Radio Label</label>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
--- components/index.php
+++ components/index.php
@@ -1,31 +1,16 @@
 <?php include '../components/head.php';?>
 <body>
-  <div class="bc-pageheader text-center">
-    <a href="#"><h1 id="style-header" class="display-3"> <img src="./imgs/blue-logo-blockchain.svg" style="height: 100px;margin-bottom: 15px;"> Style Guide </h1></a>
+  <div class="">
+    <h1 class="blue em-200 f-36 mb-30 hidden-xs ph-80"> Blockchain Wallet — 2017 — elements </h1>
   </div>
-
-  <div class="container guide-items flex-column">
-    <div class="flex-row">
-      <a href="#buttons"><span class="ti-link"></span></a>
-      <?php include 'buttons.php';?>
-    </div>
-    <hr>
-    <div class="flex-row">
-      <a href="#colors"><span class="ti-link"></span></a>
-      <?php include 'colors.php';?>
-    </div>
-    <hr>
-    <div class="flex-row">
-      <a href="#fonts"><span class="ti-link"></span></a>
-      <?php include 'fonts.php';?>
-    </div>
-    <hr>
-    <div class="flex-row">
-      <a href="#forms"><span class="ti-link"></span></a>
-      <?php include 'forms.php';?>
-    </div>
+  <div class="visible-xs text-center">
+    <h1 class="blue em-200 f-36"> Blockchain Wallet </h1>
+    <h1 class="blue em-200 f-36 mtn"> 2017 — elements </h1>
   </div>
-
+  <div class="guide-items flex-column width-100 phn mhn">
+    <?php include '../components/elementpanel1.php';?>
+    <?php include '../components/elementpanel2.php';?>
+  </div>
   <script src="./js/plugins/jquery-3.2.1.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="./js/app.js"></script>
